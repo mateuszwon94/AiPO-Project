f5af636fff18311be46a1a1c23a3df0c41a63892
Mateuszwon94 <mateusz.winiarski.94@gmail.com>
2017-04-28 17:45:34 +0200

    Add button to main window

--- AiPO-Project/main.cpp
+++ AiPO-Project/main.cpp
@@ -1,7 +1,11 @@
 #include <QtCore/QCoreApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[]) {
 	QCoreApplication a(argc, argv);
+
+	QPushButton button("Hello world !");
+	button.show();
 
 	return a.exec();
 }
