453fdd343de285fc98ac62f22512754d2e9158d4
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2017-06-15 10:25:16 +0200

    Create simplelayout

--- AiPO-Project/GeneratedFiles/ui_MainWindow.h
+++ AiPO-Project/GeneratedFiles/ui_MainWindow.h
@@ -14,8 +14,11 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -25,9 +28,17 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindowClass
 {
 public:
+    QWidget *centralWidget;
+    QPushButton *AddLeft_Button;
+    QPushButton *RemoveLeft_Button;
+    QPushButton *AddRight_Button;
+    QPushButton *RemoveRigth_Button;
+    QSlider *Adjust_Slider;
+    QLabel *label;
+    QPushButton *SaveImage_Button;
+    QPushButton *SaveAnimation_Button;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
-    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindowClass)
@@ -35,15 +46,46 @@ public:
         if (MainWindowClass->objectName().isEmpty())
             MainWindowClass->setObjectName(QStringLiteral("MainWindowClass"));
         MainWindowClass->resize(600, 400);
+        centralWidget = new QWidget(MainWindowClass);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        AddLeft_Button = new QPushButton(centralWidget);
+        AddLeft_Button->setObjectName(QStringLiteral("AddLeft_Button"));
+        AddLeft_Button->setGeometry(QRect(10, 10, 71, 31));
+        RemoveLeft_Button = new QPushButton(centralWidget);
+        RemoveLeft_Button->setObjectName(QStringLiteral("RemoveLeft_Button"));
+        RemoveLeft_Button->setGeometry(QRect(90, 10, 71, 31));
+        AddRight_Button = new QPushButton(centralWidget);
+        AddRight_Button->setObjectName(QStringLiteral("AddRight_Button"));
+        AddRight_Button->setGeometry(QRect(522, 10, 71, 31));
+        RemoveRigth_Button = new QPushButton(centralWidget);
+        RemoveRigth_Button->setObjectName(QStringLiteral("RemoveRigth_Button"));
+        RemoveRigth_Button->setGeometry(QRect(442, 10, 71, 31));
+        Adjust_Slider = new QSlider(centralWidget);
+        Adjust_Slider->setObjectName(QStringLiteral("Adjust_Slider"));
+        Adjust_Slider->setGeometry(QRect(170, 20, 261, 21));
+        Adjust_Slider->setOrientation(Qt::Horizontal);
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(270, 10, 55, 16));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy);
+        SaveImage_Button = new QPushButton(centralWidget);
+        SaveImage_Button->setObjectName(QStringLiteral("SaveImage_Button"));
+        SaveImage_Button->setGeometry(QRect(10, 300, 171, 28));
+        SaveAnimation_Button = new QPushButton(centralWidget);
+        SaveAnimation_Button->setObjectName(QStringLiteral("SaveAnimation_Button"));
+        SaveAnimation_Button->setGeometry(QRect(420, 300, 171, 28));
+        MainWindowClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindowClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 600, 26));
         MainWindowClass->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindowClass);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindowClass->addToolBar(mainToolBar);
-        centralWidget = new QWidget(MainWindowClass);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        MainWindowClass->setCentralWidget(centralWidget);
+        MainWindowClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(MainWindowClass);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindowClass->setStatusBar(statusBar);
@@ -56,6 +98,13 @@ public:
     void retranslateUi(QMainWindow *MainWindowClass)
     {
         MainWindowClass->setWindowTitle(QApplication::translate("MainWindowClass", "MainWindow", Q_NULLPTR));
+        AddLeft_Button->setText(QApplication::translate("MainWindowClass", "Dodaj", Q_NULLPTR));
+        RemoveLeft_Button->setText(QApplication::translate("MainWindowClass", "Usu\305\204", Q_NULLPTR));
+        AddRight_Button->setText(QApplication::translate("MainWindowClass", "Dodaj", Q_NULLPTR));
+        RemoveRigth_Button->setText(QApplication::translate("MainWindowClass", "Usu\305\204", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindowClass", "Dopasuj", Q_NULLPTR));
+        SaveImage_Button->setText(QApplication::translate("MainWindowClass", "Zapisz obraz wynikowy", Q_NULLPTR));
+        SaveAnimation_Button->setText(QApplication::translate("MainWindowClass", "Zapisz animacj\304\231 wynikow\304\205", Q_NULLPTR));
     } // retranslateUi
 
 };
